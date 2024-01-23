14c69167c09f7f1c157e6a95f40c0d7443f1650c
yicode <120433484+yuki-845@users.noreply.github.com>
2024-01-23 14:58:17 +0900

    kk

--- regex_app/urls.py
+++ regex_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
+app_name = "regex_app"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.confirm, name="confirm"),
 ]
--- regex_app/views.py
+++ regex_app/views.py
@@ -1,19 +1,8 @@
-from django.urls import reverse_lazy
-from django.views.generic.edit import FormView
+
 from django.shortcuts import render
 
 
 def confirm(request):
-    message = 'データ受け取ったよ！'
-    if request.method == 'POST':
-        nyuryoku1 = request.POST['nyuryoku1']
-        nyuryoku2 = request.POST['nyuryoku2']
-        nyuryoku3 = request.POST['nyuryoku3']
-
-        params = {
-            "nyuryoku1": nyuryoku1,
-            "nyuryoku2": nyuryoku2,
-            "nyuryoku3": nyuryoku3,
-            "message": message,
-        }
-        return render(request, '', params)
+    
+    if request.method == 'GET':
+        return render(request, 'regex_app/index.html')
